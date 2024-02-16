ca48a00d3d95b48fe13c5fc430b02a381abdad42
Michel <michel.recchia3@etu.univ-lorraine.fr>
2024-02-16 08:36:24 +0100

    jsp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA-JS753EnRXFVUQox377zXZ95iUIw6mME',
+    authDomain: 'ionic-65a96.firebaseapp.com',
+    projectId: 'ionic-65a96',
+    storageBucket: 'ionic-65a96.appspot.com',
+    messagingSenderId: '997117114662',
+    appId: '1:997117114662:web:9833ee7d5de6d6cd7ba9e7',
+    measurementId: 'G-29T8DEQ2Y6',
+  },
 };
